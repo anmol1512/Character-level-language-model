ea360b516477f8d5337d5cb23d1678d340046758
anmol chhetri <anmol1512@gmail.com>
2024-01-14 02:16:33 +0530

    setup config logging utility

--- utils.py
+++ utils.py
@@ -5,6 +5,23 @@ import pyyaml
 import json
 
 
+
+def setup_logging(config):
+    # working directory specified in default_config
+    work_dir=config.system.work_dir
+    #created a directory where we will log our configurations
+    os.makedirs(work_dir,exist_ok=True) 
+    
+    # write the current command line argument in cli_args.txt file
+    with open(os.path.join(work_dir+'cli_args.txt'),'w') as f:
+        f.write(' '.sys.argv)
+    
+    #create and write the current configuration in a yaml file config.yaml
+    with open(os.path.join(work_dir+'config.yaml'),'w') as f:
+        yaml.dump(config._dict_repr(),f,default_flow_style=False)
+
+
+
 ''' A lightweight configuration system inspired by yacs[yet another configuration system] '''
 class CfgNode:
     #Below are the target of this class
